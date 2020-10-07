ffdea9f1c7f0ce367fd170f8064c0f5a89848540
Lily <lilyau0112@gmail.com>
2020-10-07 15:23:58 +0800

    store and display note

--- app.js
+++ app.js
@@ -1,40 +1,91 @@
 class App {
-    constructor() {
-      this.$form = document.querySelector('#form');
-      this.$noteTitle = document.querySelector('#note-title');
-      this.$formButtons = document.querySelector('#form-buttons');
-      
-      this.addEventListeners();
-    }
-    
-    addEventListeners() {
-      document.body.addEventListener('click', event => {
-        this.handleFormClick(event);
-      });
-    }
-    
-    handleFormClick(event) {
-      const isFormClicked = this.$form.contains(event.target); 
-      
-      if (isFormClicked) {
-        this.openForm();
-      } else {
-        this.closeForm();
+  constructor() {
+    this.notes = []; //store user notes as array
+
+
+    //dom element indicated start with $
+    this.$placeholder = document.querySelector('#placeholder');
+    this.$form = document.querySelector('#form');
+    this.$notes = document.querySelector('#notes');
+    this.$noteTitle = document.querySelector('#note-title');
+    this.$noteText = document.querySelector('#note-text');
+    this.$formButtons = document.querySelector('#form-buttons');
+
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    document.body.addEventListener('click', event => {
+      this.handleFormClick(event);
+    });
+
+    this.$form.addEventListener('submit', event => {
+      event.preventDefault();
+      const title = this.$noteTitle.value;
+      const text = this.$noteText.value;
+
+      if (title || text) {
+        this.addNote({ text, title });
       }
+    });
+  }
+
+  handleFormClick(event) {
+    const isFormClicked = this.$form.contains(event.target);
+
+    if (isFormClicked) {
+      this.openForm();
+    } else {
+      this.closeForm();
     }
-    
-    openForm() {
-      this.$form.classList.add('form-open');  
-      this.$noteTitle.style.display = 'block';
-      this.$formButtons.style.display = 'block';
-    }
-    
-    closeForm() {
-      this.$form.classList.remove('form-open');  
-      this.$noteTitle.style.display = 'none';
-      this.$formButtons.style.display = 'none';  
-    }
+  }
+
+  openForm() {
+    this.$form.classList.add('form-open');
+    this.$noteTitle.style.display = 'block';
+    this.$formButtons.style.display = 'block';
+  }
+
+  closeForm() {
+    this.$form.classList.remove('form-open');
+    this.$noteTitle.style.display = 'none';
+    this.$formButtons.style.display = 'none';
+    this.$noteText.value = '';
+    this.$noteTitle.value = '';
+  }
+
+  addNote(note) {
+    const newNote = {
+      title: note.title,
+      text: note.text,
+      color: "white",
+      id: this.notes.length > 0 ?
+        this.notes[this.notes.length - 1].id + 1 :
+        1
+    };
+    this.notes = [...this.notes, newNote];
+    this.displayNotes();
+    this.closeForm();
+  }
+
+  displayNotes() {
+    const hasNotes = this.notes.length > 0;
+    this.$placeholder.style.display = hasNotes ? 'none' : 'flex';
+
+    this.$notes.innerHTML = this.notes.map(note => `
+        <div style = background : ${note.color}; class="note">
+            <div class="${note.title && 'note-title'}">${note.title} </div>
+            <div class="note-text">${note.text}</div>
+            <div class="toolbar-container">
+            <div class="toolbar">
+              <img class="toolbar-color" src="https://icon.now.sh/palette">
+              <img class="toolbar-delete" src="https://icon.now.sh/delete">
+            </div>
+          </div>
+        </div>
+        ` ).join("");
+    //line74: if note.title is truy, then set class = 'note-title'
+  }
 }
-  
+
 new App();
-  
